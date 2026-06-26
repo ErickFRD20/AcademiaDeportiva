09dcfd568fc1e184aadfb49e87cbf7ef1db1113b
ErickFRD <erreyesde@est.utn.ac.cr>
2026-06-26 15:55:51 -0600

    Clase persona
    Se crea la clase persona

--- src/persona/Persona.java
+++ src/persona/Persona.java
@@ -3,11 +3,48 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package persona;
-
+import java.time.LocalDate;
 /**
  *
  * @author Student
  */
 public class Persona {
+    protected String id;
+    protected String nombre;
+    protected LocalDate fechaNacimiento;
+    protected String telefono;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Persona(String id, String nombre, LocalDate fechaNacimiento, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "id=" + id + ", nombre=" + nombre + ", fechaNacimiento=" + fechaNacimiento + ", telefono=" + telefono + '}';
+    }
+    
     
 }
